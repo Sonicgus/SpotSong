a357050182028b73b1dfb057a817210faaceba55
Gustavo Lima <gustavo.samuel.lima@gmail.com>
2023-06-12 16:23:06 +0100

    inserido um consumidor

--- sql/insert_data.sql
+++ sql/insert_data.sql
@@ -17,4 +17,8 @@ INSERT INTO artist_song (artist_person_users_id, song_ismn) VALUES (2, 1);
 
 INSERT INTO album (title, release_date, artist_person_users_id, label_id) VALUES ('classicos dos anos 90','2023-06-11 10:30:00',2,1);
 
-INSERT INTO song_album (song_ismn, album_id) VALUES (1,1)
+INSERT INTO song_album (song_ismn, album_id) VALUES (1,1);
+
+INSERT INTO users (username, password, email) VALUES ('quim', 'quim', 'quim@example.com');
+INSERT INTO person (address, number, users_id) SELECT 'rua fulano de tal', '969 999 999', id FROM users WHERE username = 'quim';
+INSERT INTO consumer (person_users_id) SELECT id FROM users WHERE username = 'quim';
