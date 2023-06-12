bcd8dc673e95b9887b4184291fa5ea273c9d4a33
Gustavo Lima <gustavo.samuel.lima@gmail.com>
2023-06-12 22:52:19 +0100

    Update insert_data.sql

--- sql/insert_data.sql
+++ sql/insert_data.sql
@@ -12,11 +12,9 @@ INSERT INTO person (address, contact, users_id) SELECT 'rua fulano de tal', '969
 INSERT INTO artist (administrator_users_id, artistic_name, label_id, person_users_id) SELECT 1, 'bluedays', l.id, u.id FROM label l, users u WHERE l.name = 'soni' AND u.username = 'blueday';
 
 INSERT INTO song (title, release_date, duration, genre, artist_person_users_id, label_id) VALUES ('opa ganda style','2023-06-11 10:30:00','2023-06-11 10:30:00','rap',2,1);
-
 INSERT INTO artist_song (artist_person_users_id, song_ismn) VALUES (2, 1);
 
 INSERT INTO album (title, release_date, artist_person_users_id, label_id) VALUES ('classicos dos anos 90','2023-06-11 10:30:00',2,1);
-
 INSERT INTO song_album (song_ismn, album_id) VALUES (1,1);
 
 INSERT INTO users (username, password, email) VALUES ('quim', 'quim', 'quim@example.com');
