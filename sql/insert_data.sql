7041275b685c909488112736a8f78bb8c0d98781
Gustavo Lima <gustavo.samuel.lima@gmail.com>
2023-06-15 16:11:35 +0100

    update er

--- sql/insert_data.sql
+++ sql/insert_data.sql
@@ -1,22 +1,162 @@
-INSERT INTO label (name) VALUES ('soni');
+CREATE TABLE users (
+	id	 BIGSERIAL,
+	username VARCHAR(30) NOT NULL,
+	password VARCHAR(512) NOT NULL,
+	email	 VARCHAR(255) NOT NULL,
+	PRIMARY KEY(id)
+);
 
-INSERT INTO users (username, password, email) VALUES ('admin', '$2b$12$hah5bfkByhXdgznLvrD1Fete.6.nLbTH5jmCDX64xLWtMB9/dCkDi', 'admin@example.com');
-INSERT INTO administrator (users_id) SELECT id FROM users WHERE username = 'admin';
+CREATE TABLE consumer (
+	person_users_id BIGINT,
+	PRIMARY KEY(person_users_id)
+);
 
-INSERT INTO users (username, password, email) VALUES ('artista', '$2b$12$.SB/6QQsnrsMJgUSB2Dx4.kE1P2I.zk1vMIt1y1AntkiQEH07NQBG', 'artista@example.com');
-INSERT INTO person (address, contact, users_id) SELECT 'rua fulano de tal', '969 999 999', id FROM users WHERE username = 'artista';
-INSERT INTO artist (administrator_users_id, artistic_name, label_id, person_users_id) SELECT 1,'artisteiro', l.id, u.id FROM label l, users u WHERE l.name = 'soni' AND u.username = 'artista';
+CREATE TABLE artist (
+	artistic_name		 VARCHAR(512) NOT NULL,
+	administrator_users_id BIGINT NOT NULL,
+	label_id		 BIGINT NOT NULL,
+	person_users_id	 BIGINT,
+	PRIMARY KEY(person_users_id)
+);
 
-INSERT INTO users (username, password, email) VALUES ('blueday', '$2b$12$etXXhBH28Unqkg9e4TJVj.HBI5coShx9TWK8r61jD35G4LR2Si4KW', 'blueday@example.com');
-INSERT INTO person (address, contact, users_id) SELECT 'rua fulano de tal', '969 999 999', id FROM users WHERE username = 'blueday';
-INSERT INTO artist (administrator_users_id, artistic_name, label_id, person_users_id) SELECT 1, 'bluedays', l.id, u.id FROM label l, users u WHERE l.name = 'soni' AND u.username = 'blueday';
+CREATE TABLE administrator (
+	users_id BIGINT,
+	PRIMARY KEY(users_id)
+);
 
-INSERT INTO song (title, release_date, duration, genre, artist_person_users_id, label_id) VALUES ('opa ganda style','2023-06-11 10:30:00','2023-06-11 10:30:00','rap',2,1);
-INSERT INTO artist_song (artist_person_users_id, song_ismn) VALUES (2, 1);
+CREATE TABLE playlist (
+	id			 BIGSERIAL,
+	name			 VARCHAR(512) NOT NULL,
+	is_private		 BOOL,
+	consumer_person_users_id BIGINT NOT NULL,
+	PRIMARY KEY(id)
+);
 
-INSERT INTO album (title, release_date, artist_person_users_id, label_id) VALUES ('classicos dos anos 90','2023-06-11 10:30:00',2,1);
-INSERT INTO song_album (song_ismn, album_id) VALUES (1,1);
+CREATE TABLE comment (
+	id			 BIGSERIAL,
+	text			 VARCHAR(512),
+	song_ismn		 BIGINT NOT NULL,
+	consumer_person_users_id BIGINT NOT NULL,
+	comment_id		 BIGINT,
+	PRIMARY KEY(id)
+);
 
-INSERT INTO users (username, password, email) VALUES ('quim', '$2b$12$uSYksKvXjCiumRxqX.f1auBml2fvD7BXcPuPpF9NGNhHV2NMg.qAa', 'quim@example.com');
-INSERT INTO person (address, contact, users_id) SELECT 'rua fulano de tal', '969 999 999', id FROM users WHERE username = 'quim';
-INSERT INTO consumer (person_users_id) SELECT id FROM users WHERE username = 'quim';
+CREATE TABLE label (
+	id	 BIGSERIAL,
+	name VARCHAR(512) NOT NULL,
+	PRIMARY KEY(id)
+);
+
+CREATE TABLE song (
+	ismn			 BIGSERIAL,
+	title			 VARCHAR(512) NOT NULL,
+	release_date		 TIMESTAMP NOT NULL,
+	duration		 VARCHAR(512) NOT NULL,
+	genre			 VARCHAR(512) NOT NULL,
+	artist_person_users_id BIGINT NOT NULL,
+	label_id		 BIGINT NOT NULL,
+	PRIMARY KEY(ismn)
+);
+
+CREATE TABLE person (
+	address	 VARCHAR(512) NOT NULL,
+	contact	 VARCHAR(512) NOT NULL,
+	users_id BIGINT,
+	PRIMARY KEY(users_id)
+);
+
+CREATE TABLE album (
+	id			 BIGSERIAL,
+	title			 VARCHAR(512) NOT NULL,
+	release_date		 TIMESTAMP NOT NULL,
+	artist_person_users_id BIGINT NOT NULL,
+	label_id		 BIGINT NOT NULL,
+	PRIMARY KEY(id)
+);
+
+CREATE TABLE card (
+	id			 BIGSERIAL,
+	code			 VARCHAR(16) NOT NULL,
+	expire		 TIMESTAMP NOT NULL,
+	amount		 INTEGER NOT NULL,
+	type			 INTEGER NOT NULL,
+	administrator_users_id BIGINT NOT NULL,
+	PRIMARY KEY(id)
+);
+
+CREATE TABLE view (
+	id			 BIGSERIAL,
+	date_view		 TIMESTAMP NOT NULL,
+	song_ismn		 BIGINT,
+	consumer_person_users_id BIGINT,
+	PRIMARY KEY(id,song_ismn,consumer_person_users_id)
+);
+
+CREATE TABLE history_card (
+	cost			 INTEGER NOT NULL,
+	purchase_subscription_id BIGINT,
+	card_id			 BIGINT,
+	PRIMARY KEY(purchase_subscription_id,card_id)
+);
+
+CREATE TABLE purchase_subscription (
+	id			 BIGSERIAL,
+	purchase_date		 TIMESTAMP NOT NULL,
+	subscription_id		 BIGSERIAL NOT NULL,
+	subscription_plan	 INTEGER NOT NULL,
+	subscription_init_date	 TIMESTAMP NOT NULL,
+	subscription_end_date	 TIMESTAMP NOT NULL,
+	consumer_person_users_id BIGINT NOT NULL,
+	PRIMARY KEY(id)
+);
+
+CREATE TABLE playlist_song (
+	playlist_id BIGINT,
+	song_ismn	 BIGINT,
+	PRIMARY KEY(playlist_id,song_ismn)
+);
+
+CREATE TABLE song_album (
+	song_ismn BIGINT,
+	album_id	 BIGINT,
+	PRIMARY KEY(song_ismn,album_id)
+);
+
+CREATE TABLE artist_song (
+	artist_person_users_id BIGINT,
+	song_ismn		 BIGINT,
+	PRIMARY KEY(artist_person_users_id,song_ismn)
+);
+
+ALTER TABLE users ADD UNIQUE (username);
+ALTER TABLE consumer ADD CONSTRAINT consumer_fk1 FOREIGN KEY (person_users_id) REFERENCES person(users_id);
+ALTER TABLE artist ADD UNIQUE (artistic_name);
+ALTER TABLE artist ADD CONSTRAINT artist_fk1 FOREIGN KEY (administrator_users_id) REFERENCES administrator(users_id);
+ALTER TABLE artist ADD CONSTRAINT artist_fk2 FOREIGN KEY (label_id) REFERENCES label(id);
+ALTER TABLE artist ADD CONSTRAINT artist_fk3 FOREIGN KEY (person_users_id) REFERENCES person(users_id);
+ALTER TABLE administrator ADD CONSTRAINT administrator_fk1 FOREIGN KEY (users_id) REFERENCES users(id);
+ALTER TABLE playlist ADD CONSTRAINT playlist_fk1 FOREIGN KEY (consumer_person_users_id) REFERENCES consumer(person_users_id);
+ALTER TABLE comment ADD CONSTRAINT comment_fk1 FOREIGN KEY (song_ismn) REFERENCES song(ismn);
+ALTER TABLE comment ADD CONSTRAINT comment_fk2 FOREIGN KEY (consumer_person_users_id) REFERENCES consumer(person_users_id);
+ALTER TABLE comment ADD CONSTRAINT comment_fk3 FOREIGN KEY (comment_id) REFERENCES comment(id);
+ALTER TABLE label ADD UNIQUE (name);
+ALTER TABLE song ADD CONSTRAINT song_fk1 FOREIGN KEY (artist_person_users_id) REFERENCES artist(person_users_id);
+ALTER TABLE song ADD CONSTRAINT song_fk2 FOREIGN KEY (label_id) REFERENCES label(id);
+ALTER TABLE person ADD CONSTRAINT person_fk1 FOREIGN KEY (users_id) REFERENCES users(id);
+ALTER TABLE album ADD CONSTRAINT album_fk1 FOREIGN KEY (artist_person_users_id) REFERENCES artist(person_users_id);
+ALTER TABLE album ADD CONSTRAINT album_fk2 FOREIGN KEY (label_id) REFERENCES label(id);
+ALTER TABLE card ADD UNIQUE (code);
+ALTER TABLE card ADD CONSTRAINT card_fk1 FOREIGN KEY (administrator_users_id) REFERENCES administrator(users_id);
+ALTER TABLE view ADD CONSTRAINT view_fk1 FOREIGN KEY (song_ismn) REFERENCES song(ismn);
+ALTER TABLE view ADD CONSTRAINT view_fk2 FOREIGN KEY (consumer_person_users_id) REFERENCES consumer(person_users_id);
+ALTER TABLE history_card ADD CONSTRAINT history_card_fk1 FOREIGN KEY (purchase_subscription_id) REFERENCES purchase_subscription(id);
+ALTER TABLE history_card ADD CONSTRAINT history_card_fk2 FOREIGN KEY (card_id) REFERENCES card(id);
+ALTER TABLE purchase_subscription ADD UNIQUE (subscription_id);
+ALTER TABLE purchase_subscription ADD CONSTRAINT purchase_subscription_fk1 FOREIGN KEY (consumer_person_users_id) REFERENCES consumer(person_users_id);
+ALTER TABLE playlist_song ADD CONSTRAINT playlist_song_fk1 FOREIGN KEY (playlist_id) REFERENCES playlist(id);
+ALTER TABLE playlist_song ADD CONSTRAINT playlist_song_fk2 FOREIGN KEY (song_ismn) REFERENCES song(ismn);
+ALTER TABLE song_album ADD CONSTRAINT song_album_fk1 FOREIGN KEY (song_ismn) REFERENCES song(ismn);
+ALTER TABLE song_album ADD CONSTRAINT song_album_fk2 FOREIGN KEY (album_id) REFERENCES album(id);
+ALTER TABLE artist_song ADD CONSTRAINT artist_song_fk1 FOREIGN KEY (artist_person_users_id) REFERENCES artist(person_users_id);
+ALTER TABLE artist_song ADD CONSTRAINT artist_song_fk2 FOREIGN KEY (song_ismn) REFERENCES song(ismn);
+
